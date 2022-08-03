9bf4429ae9b231365f33a999f445c7cdc7200f96
Roman Alexandrovich <87494379+Aristosya@users.noreply.github.com>
2022-08-03 17:51:38 +0300

    removed hash from url

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lecon/services/firebase_auth_methods.dart';
 import 'package:provider/provider.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'firebase_options.dart';
 import 'route_generator.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,6 +16,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setPathUrlStrategy();
   runApp(MultiProvider(
     providers: [
       Provider<FirebaseAuthMethods>(
